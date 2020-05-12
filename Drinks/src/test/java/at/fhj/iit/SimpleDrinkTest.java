2598fc2d0d2a9ee1a6550f4929021e1881af3e55
DavidFeirer <david.feirer@edu.campus02.at>
2020-05-12 15:12:29 +0200

    creating the SimpleDrinkTest and WineTest

---
+++ Drinks/src/test/java/at/fhj/iit/SimpleDrinkTest.java
@@ -0,0 +1,7 @@
+package at.fhj.iit;
+
+import org.junit.jupiter.api.DisplayName;
+
+@DisplayName("Testing SimpleDrink implementation")
+public class SimpleDrinkTest {
+}
---
+++ Drinks/src/test/java/at/fhj/iit/WineTest.java
@@ -0,0 +1,7 @@
+package at.fhj.iit;
+
+import org.junit.jupiter.api.DisplayName;
+
+@DisplayName("Testing Wine implementation")
+public class WineTest {
+}
