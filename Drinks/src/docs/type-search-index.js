3fed2f5f3096620c6ae0a473de200919cd454eb1
DavidFeirer <david.feirer@edu.campus02.at>
2020-05-13 12:02:55 +0200

    adding the JavaDoc

---
+++ Drinks/src/docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"at.fhj.iit","l":"Drink"},{"p":"at.fhj.iit","l":"Liquid"},{"p":"at.fhj.iit","l":"LiquidTest"},{"p":"at.fhj.iit","l":"SimpleDrink"},{"p":"at.fhj.iit","l":"SimpleDrinkTest"},{"p":"at.fhj.iit","l":"TempException"},{"p":"at.fhj.iit","l":"Wine"},{"p":"at.fhj.iit","l":"WineTest"}]
